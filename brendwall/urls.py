de71cd6ae1ab59540b8007bc86e2c8d749137f99
SearchingFox <n3tstrang3r@yandex.ru>
2024-09-12 06:01:40 +0300

    Add frontend

--- brendwall/urls.py
+++ brendwall/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from products.views import IndexView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
     path('api/', include('products.urls'), name='products'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import TemplateView
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductListSerializer, ProductCreateSerializer
@@ -7,5 +8,10 @@ class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
 
+
 class ProductCreateView(generics.CreateAPIView):
     serializer_class = ProductCreateSerializer
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
